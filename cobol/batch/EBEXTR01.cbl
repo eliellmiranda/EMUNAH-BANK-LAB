19eee86257c6b07b96b08fce602c82032e085294
Eliel Miranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2026-04-30 18:47:47 -0300

    Update EBEXTR01.cbl

--- cobol/batch/EBEXTR01.cbl
+++ cobol/batch/EBEXTR01.cbl
@@ -115,7 +115,7 @@
       * Arquivo de auditoria  layout via CPAUD001                    *
       *---------------------------------------------------------------*
        FD  AUDIT-OUT
-           RECORD CONTAINS 120 CHARACTERS
+           RECORD CONTAINS 128 CHARACTERS
            RECORDING MODE IS F.
        01  AUDIT-REG.
            COPY CPAUD001.
