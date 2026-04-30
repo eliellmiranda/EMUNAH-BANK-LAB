d726c1238db83c249dd0467f691883c5b35bed3a
Eliel Miranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2026-04-30 20:03:19 -0300

    Update EBREPR01.cbl

--- cobol/batch/EBREPR01.cbl
+++ cobol/batch/EBREPR01.cbl
@@ -119,7 +119,7 @@
       * Registro de auditoria - layout padrao CPAUD001 (120 bytes)    *
       *---------------------------------------------------------------*
        FD  AUDIT-OUT
-           RECORD CONTAINS 156 CHARACTERS
+           RECORD CONTAINS 128 CHARACTERS
            RECORDING MODE IS F.
        01  AUDIT-REG.
            COPY CPAUD001.
