8ef17b429ac233dac1039ffc34a4bd3a7e3d675e
Eliel Miranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2026-05-03 11:03:02 -0300

    Update EBCONC01.cbl

--- cobol/batch/EBCONC01.cbl
+++ cobol/batch/EBCONC01.cbl
@@ -29,7 +29,7 @@
       * CORRECAO: Removido prefixo AS- pois trata-se de um VSAM ESDS  *
       *---------------------------------------------------------------*
            SELECT MOVTO-IN
-               ASSIGN TO MOVTIN
+               ASSIGN TO AS-MOVTIN
                ORGANIZATION IS SEQUENTIAL
                ACCESS MODE IS SEQUENTIAL
                FILE STATUS IS WS-FS-MOVTIN.
